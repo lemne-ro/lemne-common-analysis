bcc8ff23fac4e8ebf8c992e7934d41c1082b8d1c
vl <vlad.licaret@gmail.com>
2025-06-24 23:27:42 +0300

    ultralytics training

--- lemne_common/vision/ultralytics_model.py
+++ lemne_common/vision/ultralytics_model.py
@@ -1,10 +1,12 @@
+import os
+from shutil import move
 import numpy as np
 from ultralytics import YOLO
 import cv2
 
 
 class UltralyticsModel():
-    def __init__(self, model_type, classes, checkpoint, image_size=640):
+    def __init__(self, model_type, classes, checkpoint, image_size=640):    # TODO change default size
         super().__init__()
 
         self.model_type = model_type
@@ -16,6 +18,24 @@ class UltralyticsModel():
             self.model = YOLO(checkpoint)
         else:
             raise NotImplementedError
+    
+    @staticmethod
+    def train(dataset_path, output_path, model='yolov11s.pt', image_size=224, device='cuda', epochs=50, batch=16):
+        model = YOLO(model)
+
+        result = model.train(
+            data=dataset_path,
+            # save_dir=output_path, # fail
+            epochs=epochs,
+            imgsz=image_size,
+            batch=batch,
+            device=device,
+        )
+
+        # fix save dir
+        move(os.path.join(os.getcwd(), result.save_dir), output_path)
+
+        return result
         
     def run(self, image):
         multi = image.ndim > 3
@@ -25,6 +45,8 @@ class UltralyticsModel():
 
         if not multi:
             return results[0]
+        
+        # TODO standard API for results
             
         return results
 
